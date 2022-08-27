1747e0fb18aa0d9ea6d7516c7556af3664c2f9db
Russel Rai <leon.amorphy@googlemail.com>
2022-08-27 17:44:08 +0100

    add put request to change items in database

--- src/App.js
+++ src/App.js
@@ -38,6 +38,17 @@ function App() {
     setNewNote(event.target.value)
   }
 
+  const toggleImportanceOf = (id) => {
+    console.log(`importance of ${id} needs to be toggled`)
+    const url = `${baseURL}/notes/${id}`
+    const note = notes.find((note) => note.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : response.data)))
+    })
+  }
+
   const notesToShow = showAll
     ? notes
     : notes.filter((note) => note.important === true)
@@ -55,6 +66,7 @@ function App() {
           <Note
             key={note.id}
             note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
           />
         ))}
       </ul>
