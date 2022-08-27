ffed5cb8d56f16c23885fea8dbbc0a932e5ca49b
Russel Rai <leon.amorphy@googlemail.com>
2022-08-27 18:27:35 +0100

    add services to its own folder

---
+++ src/services/NotesService.js
@@ -0,0 +1,16 @@
+const baseURL = 'http://localhost:3001/notes'
+
+const getAll = () => {
+  const request = axios.get(`${baseURL}`)
+  return request.then((response) => response.data)
+}
+
+const createNote = (noteObject) => {
+  const request = axios.post(`${baseURL}`, noteObject)
+  return request.then((response) => response.data)
+}
+
+const updateNoteImportance = (id, changedNote) => {
+  const request = axios.put(`${baseURL}/${id}`, changedNote)
+  return request.then((response) => response.data)
+}
